34a817d3352758a11a208d75c3de4ec239b2d3d1
Victor Goya <goya.victor@gmail.com>
2018-12-12 18:04:42 +0100

    Update video.rb

--- lib/yt/models/video.rb
+++ lib/yt/models/video.rb
@@ -37,6 +37,9 @@ module Yt
       #   @return [String] the title of the channel that the video belongs to.
       delegate :channel_title, to: :snippet
 
+      delegate :default_language, to: :snippet
+      delegate :default_audio_language, to: :snippet
+      
       # @return [<String>] the URL of the channel that the video belongs to.
       def channel_url
         "https://www.youtube.com/channel/#{channel_id}"
